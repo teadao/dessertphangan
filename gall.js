46bbd0e034d032c753421c6ab67f186d072c64a7
teadao <72756410+teadao@users.noreply.github.com>
2024-10-20 01:10:23 +0700

    Update gall.js

--- gall.js
+++ gall.js
@@ -1,50 +1,21 @@
-var expandedImg = null;
+var expandedImg = null; 
 var imgText = null;
 
 function myFunction(imgs) {
-    if (expandedImg !== null) {
-        expandedImg.parentElement.style.display = "none";
-    }
-    // Get the expanded image
+    // Get the modal and expanded image elements
+    var modal = document.getElementById("galleryModal");
     expandedImg = document.getElementById("gallery_expandedImg");
-    // Get the image text
     imgText = document.getElementById("gallery_imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
+
+    // Use the same src and alt attributes
     expandedImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
     imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandedImg.parentElement.style.display = "block";
+
+    // Display the modal
+    modal.style.display = "block";
 }
 
-function myFunction1(imgs) {
-    if (expandedImg !== null) {
-        expandedImg.parentElement.style.display = "none";
-    }
-    // Get the expanded image
-    expandedImg = document.getElementById("gallery_expandedImg1");
-    // Get the image text
-    imgText = document.getElementById("gallery_imgtext1");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandedImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandedImg.parentElement.style.display = "block";
-}
-
-function myFunction2(imgs) {
-    if (expandedImg !== null) {
-        expandedImg.parentElement.style.display = "none";
-    }
-    // Get the expanded image
-    expandedImg = document.getElementById("gallery_expandedImg2");
-    // Get the image text
-    imgText = document.getElementById("gallery_imgtext2");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandedImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandedImg.parentElement.style.display = "block";
+function closeModal() {
+    var modal = document.getElementById("galleryModal");
+    modal.style.display = "none";
 }
